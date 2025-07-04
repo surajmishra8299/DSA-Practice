fe4e4ad270906a7eccad62268c844a9a3120c829
Suraj Mishra <surajmishra388495@gmail.com>
2025-07-04 18:20:18 +0530

    Code

--- PYTHON/removeduplicate.py
+++ PYTHON/removeduplicate.py
@@ -8,3 +8,29 @@ def removed(arr):
 
 num = [1,1,1,2,3,1,3,4,5,2]
 print(removed(num))
+
+
+'''
+
+def remove_duplicates(arr):
+    unique = [0] * len(arr)  # create list of same size
+    index = 0
+
+    for i in range(len(arr)):
+        duplicate = False
+        for j in range(index):
+            if arr[i] == unique[j]:
+                duplicate = True
+                break
+
+        if not duplicate:
+            unique[index] = arr[i]
+            index += 1
+
+    # Only return the filled part (since rest is 0s)
+    return unique[:index]
+
+arr = [1, 1, 2, 3, 2, 4, 5, 3]
+print(remove_duplicates(arr))
+
+'''
